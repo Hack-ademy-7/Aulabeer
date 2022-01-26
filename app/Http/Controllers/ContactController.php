e5733882eac4cf376161829b0558390117a0b967
Nicola Milella <nicombk@gmail.com>
2022-01-26 10:31:01 +0100

    contacts con model y orm

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,14 +7,14 @@ use App\Mail\ContactReceived;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\ContactRequest;
-
-
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
     public function index()
     {
-        $contacts = DB::table('contacts')->get();
+        // $contacts = DB::table('contacts')->get();
+        $contacts = Contact::all();
         
         return view('contacts.index',compact('contacts'));
     }
@@ -29,7 +29,20 @@ class ContactController extends Controller
         $validated = $request->except("_token");
         
         // guardar el contacto con el query builder
-        DB::table('contacts')->insert($validated);
+        // DB::table('contacts')->insert($validated);
+
+
+        $contact = new Contact();
+
+        $contact->name=$validated['name'];
+        $contact->description=$validated['description'];
+        $contact->email=$validated['email'];
+
+        $contact->save();
+
+        // Mass assignement
+        Contact::create($validated);
+
 
         //Mail::to($validated['email'])->send(new ContactReceived($validated));
         return redirect()->route('welcome')->with("message", "Hola $request->name, hemos recibido tu mensaje <span class='text-info'>$request->description</span>, pronto te contestaremos.");
